205d1fddd1fe3369bb14b925bc6d8c7345606047
arletteosuna <arletteosuna101@gmail.com>
2024-12-06 13:58:55 -0500

    Updated finalproj1.py with new changes

--- finalproj1.py
+++ finalproj1.py
@@ -1,6 +1,7 @@
 """Class: CS230--Section 1 
 Name: Arlette Osuna
-Description: (Give a brief description for Exercise name--See below)
+Description: (Final Project: This Streamlit application allows users to explore data on New England airports through
+summary statistics, interactive queries, and map visualizations.)
 I pledge that I have completed the programming assignment independently.
 I have not copied the code from a student or any source.
 I have not given my code to any student. """
